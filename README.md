6a18802e973240a8f00b7b74d2fcbfb6fb608e92
sebasBetancourt <s3basbetan@gmail.com>
2025-08-13 12:58:21 -0500

    feat: :lipstick: Add Create User

--- README.md
+++ README.md
@@ -5,9 +5,16 @@ API REST para una Plataforma de Recetas Culinarias donde los usuarios puedan r
 
 ## Endpoints
 ### 1. Usuarios 
-1. Listar todos los Usuarios registrados
+1. Registrar nuevos usuarios en la plataforma.
+    - Metodo: POST
+    ```http://localhost:5500/usuarios/create```
+2. Listar todos los Usuarios registrados
     - Metodo: GET
     ```http://localhost:5500/usuarios/list ```
+3. Ver la información detallada de un usuario en específico.
+    
+    - Metodo GET
+    ``` http://localhost:5500/usuarios/get/:id ```
 
 ### 2. Recetas 
 ### 3. Ingredientes 
--- routers/usuariosRouter.js
+++ routers/usuariosRouter.js
@@ -1,9 +1,13 @@
 // imports
 import { Router } from "express";
 import { getDB } from "../db/config.js";
+import { ObjectId } from "bson";
 
 const router = Router();
 
+
+// Ver Todos los Usuarios
+// GET /usuarios/list
 router.get("/list", async function (req, res) {
   try {
     const usuarios = await getDB().collection("usuarios").find().toArray();
@@ -13,37 +17,43 @@ router.get("/list", async function (req, res) {
   }
 });
 
+// Ver un Usuario por ID
+// GET /usuarios/get/:id
 router.get("/get/:id", async function (req, res) {
   try {
-    const idPokemon = parseInt(req.params.id);
-    const pokemon = await getDB()
-      .collection("pokemones")
-      .findOne({ id: idPokemon });
-    if (!pokemon) {
-      res.status(404).json({ error: "Pokemon doesn't exists!" });
+    const idUsuario = parseInt(req.params.id);
+    const usuario = await getDB()
+      .collection("usuarios")
+      .findOne({ id: idUsuario });
+    if (!usuario) {
+      res.status(404).json({ error: "Usuario doesn't exists!" });
     }
-    res.status(200).json(pokemon);
+    res.status(200).json(usuario);
   } catch (er) {
     res.status(500).json({ error: "Internal server error" });
   }
 });
 
+
+// Crear un Usuario
+// POST /usuarios/create
 router.post("/create", async function (req, res) {
   try {
-    const { id, nombre, habilidad, debilitado } = req.body;
-    if (!id || !nombre || !habilidad || debilitado === undefined) {
+    const { id, nombre, email } = req.body;
+    if (!id || !nombre || !email) {
       res.status(400).json({ error: "Invalid input!" });
     }
-    const pokemon = {
+    const usuario = {
+      _id: new ObjectId(),
       id,
       nombre,
-      habilidad,
-      debilitado,
+      email,
+      recetas: [],
     };
-    await getDB().collection("usuarios").insertOne(pokemon);
-    res.status(201).json({ message: "Pokemon created!!" });
+    await getDB().collection("usuarios").insertOne(usuario);
+    res.status(201).json({ message: "User created!!" });
   } catch (error) {
-    res.error(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
